2b83570472bc00553be226764c2e686ba5d5d99c
creazyboyone <1020401390@qq.com>
2020-01-09 02:08:47 +0800

    1.优化文件显示格式 2.优化以覆盖文件形式上传文章会导致Hexo render渲染失败的bug

--- sftpFileUpload.py
+++ sftpFileUpload.py
@@ -1,5 +1,6 @@
 import paramiko
 import os
+import time
 
 
 def connect_remote():
@@ -20,6 +21,13 @@ def connect_remote():
     return ssh_client, transport
 
 
+def execute_command(c):
+    stdin, stdout, stderr = ssh.exec_command(c)
+    res, err = stdout.read(), stderr.read()
+    result = res if res else err
+    return result.decode()
+
+
 if __name__ == '__main__':
     local_dir = 'E:/markdown/blog/'
     remote_dir = r'/usr/local/blog/source/_posts/'
@@ -29,7 +37,7 @@ if __name__ == '__main__':
     print("=== sftp python v0.1 ===")
     print(" 连接服务器... ")
     ssh, sftp = connect_remote()
-    print(" 连接成功 ")
+    print(" 连接成功~")
     print(" 1.查看博客路径")
     print(" 2.上传Post文章")
     print(" 3.下载Post文章")
@@ -44,32 +52,37 @@ if __name__ == '__main__':
             exit(0)
 
         if int(answer) == 1:
-            stdin, stdout, stderr = ssh.exec_command('ls ' + remote_dir)
-            res, err = stdout.read(), stderr.read()
-            result = res if res else err
-            print(result.decode())
+            print(execute_command('ls ' + remote_dir))
 
         elif int(answer) == 2:
+            index = 1
+            files = []
             # basedir 根目录, sub_directory 根目录下所有文件夹, sub_file 根目录下所有文件
             for basedir, sub_directory, sub_file in os.walk(local_dir):
-                for i in sub_file:
-                    print(i)
-            file = input("which file want to upload?\n")
+                for value in sub_file:
+                    print(str(index) + '.' + value, end='  ')
+                    files.append(value)
+                    if index % 5 == 0:
+                        print(end='\n')
+                    index = index + 1
+            print('\n')
+            dex = input("which file want to upload?\n")
+            file = files[int(dex) - 1]
+            print(execute_command('rm ' + remote_dir + file))
+            time.sleep(1)
             sftp_client = paramiko.SFTPClient.from_transport(sftp)
-            sftp_client.put(local_dir + file + '.md', remote_dir + file + '.md')
+            sftp_client.put(local_dir + file, remote_dir + file)
+
             # md上传完后，有图片就上传图片
             if os.path.exists(local_img_dir + file):
-                stdin, stdout, stderr = ssh.exec_command('mkdir ' + remote_img_dir + file)
+                execute_command('mkdir ' + remote_img_dir + file)
                 for basedir, sub_directory, sub_file in os.walk(local_img_dir + file):
                     for i in sub_file:
                         sftp_client.put(local_img_dir + file + '/' + i, remote_img_dir + file + '/' + i)
             print("ok!\n")
 
         elif int(answer) == 3:
-            stdin, stdout, stderr = ssh.exec_command('ls ' + remote_dir)
-            res, err = stdout.read(), stderr.read()
-            result = res if res else err
-            print(result.decode())
+            print(execute_command('ls ' + remote_dir))
 
             file = input("which file want to download?\n")
             sftp_client = paramiko.SFTPClient.from_transport(sftp)
